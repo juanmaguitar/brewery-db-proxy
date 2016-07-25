033928bdfc710537cb5434b226d0a8a2d7cbd2a4
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-07-25 18:27:06 +0200

    first keypoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,34 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import BreweryDb from 'brewerydb-node'
 
 export default ({ config, db }) => {
 	let api = Router();
+	let brewdb = new BreweryDb('3ffc0422d9dfcadf2369b0a3a0ec7946');
 
+	console.log("sssss");
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
+	});
+
+	api.get('/beers', (reqApi, resApi) => {
+
+		var query = reqApi.query.q;
+
+		brewdb.search.beers({  q: query  }, function(err, res) {
+		  if (err) {
+		  	console.log (err)
+		  }
+		  if (res) {
+		  	resApi.json(res);
+		  }
+		});
+
 	});
 
 	return api;
